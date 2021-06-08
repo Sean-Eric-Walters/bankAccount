27db858613b1f7fc24d0c278be7ab59c81f461ef
Sean-Eric-Walters <seanericwalters@gmail.com>
2021-06-08 13:23:47 -0700

    ch2

--- bA.py
+++ bA.py
@@ -10,26 +10,27 @@ class BankAccount:
         return self
 
     def make_withdrawal(self, amount):
-        if self.balance<amount:
+        if self.account_balance < 0:
             print('insufficient funds: Charging a $5 fee')
             self.balance -= 5
         else:
-            self.balance -= amount
+            self.account_balance -= amount
         return self
 
     def display_account_info(self):
-        print('Balance:', self.balance)
+        print('Balance:', self.account_balance)
         return self 
 
     def yeild_interest(self):
-        if self.balance>0:
-            self.balance = self.balance(1+self.int_rate)
-            int_rate = 0.01
+        if self.account_balance>0:
+            self.int_rate = 0.01
+            self.account_balance = self.account_balance * (1+self.int_rate)
         return self
 
 Batou = BankAccount('int_rate', 'balance') 
 Major = BankAccount('int_rate', 'balance') 
 
-Batou.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(50).yeild_interest().display_account_info
+Batou.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(50).yeild_interest().display_account_info()
 
-Major.make_deposit(1000).make_deposit(2000).make_withdrawal(100).make_withdrawal(100).make_withdrawal(200).make_withdrawal(200).yeild_interest().display_account_info
+Major.make_deposit(1000).make_deposit(2000).make_withdrawal(100).make_withdrawal(100).make_withdrawal(200).make_withdrawal(200).yeild_interest().display_account_info()
+
