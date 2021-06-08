29ca8f7eba700d314e0bb6aaf0cdd3e8ab722887
Sean-Eric-Walters <seanericwalters@gmail.com>
2021-06-08 14:00:40 -0700

    ch5

--- bA.py
+++ bA.py
@@ -1,34 +1,33 @@
 class BankAccount:
     all_accounts = []
-    def __init__(self, int_rate, balance):
+    def __init__(self, int_rate, balance = 0):
         self.int_rate = int_rate
-        self.balance =  balance
-        self.account_balance = 0
+        self.balance = balance
 
     def make_deposit(self, amount):
-        self.account_balance += amount
+        self.balance += amount
         return self
 
     def make_withdrawal(self, amount):
-        if self.account_balance < 0:
+        if self.balance < 0:
             print('insufficient funds: Charging a $5 fee')
             self.balance -= 5
         else:
-            self.account_balance -= amount
+            self.balance -= amount
         return self
 
     def display_account_info(self):
-        print('Balance:', self.account_balance)
+        print('Balance:', self.balance)
         return self 
 
     def yeild_interest(self):
-        if self.account_balance>0:
+        if self.balance>0:
             self.int_rate = 0.01
-            self.account_balance = self.account_balance * (1+self.int_rate)
+            self.balance = self.balance * (1+self.int_rate)
         return self
 
-Batou = BankAccount('int_rate', 'balance') 
-Major = BankAccount('int_rate', 'balance') 
+Batou = BankAccount(0.5, 0) 
+Major = BankAccount(0.5, 0) 
 
 Batou.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(50).yeild_interest().display_account_info()
 
